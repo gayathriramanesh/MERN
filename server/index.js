ec159e4e1849a4fc4fc87ed589cad6fa2b63da03
gayathriramanesh <gayathriramanesh2001@gmail.com>
2022-06-21 23:29:25 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
-import { Express } from "express";
+import express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from './routes/posts.js';
+const app = express();
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Gayathri:mIssgAyU2001*@cluster0.htjfcwa.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL);
+app.listen(PORT, () => { console.log("server listening on port 5000") } )
 
